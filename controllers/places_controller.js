db1eb9b643a3bf1f1bb1f9bc291b76e3c8061933
Braxton Allen <braxtonallen99@yahoo.com>
2022-02-25 01:05:40 -0600

    adding Bootstrap grid to index view

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -4,13 +4,13 @@ router.get('/', (req, res) => {
     let places = [{
         name: 'Olive Garden',
         city: 'Wichita',
-        state: 'Kansas',
+        state: 'KS',
         cuisines: 'Italian food',
         pic: 'https://media.olivegarden.com/en_us/images/marketing/italian-family-restaurant-olive-garden-g6-rdv.jpg'
     }, {
         name: 'McDonald\'s',
         city: 'Hutchinson',
-        state: 'Kansas',
+        state: 'KS',
         cuisines: 'American fast food',
         pic: 'https://www.eatthis.com/wp-content/uploads/sites/4/2021/06/mcdonalds-2.jpg?quality=82&strip=1&resize=640%2C360'
     }
--- views/home.jsx
+++ views/home.jsx
@@ -9,6 +9,10 @@ function home () {
                 <a href="/places">
                     <button className='btn-primary'>Places Page</button>
                 </a>
+                <img src="/images/burger.jpg" alt="burger" />
+                <div>
+                    Photo by <a href="https://unsplash.com/@1ncreased">Lidye</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
